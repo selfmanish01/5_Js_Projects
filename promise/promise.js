bc17eca046d9284fb4acf49f0e306c2a17dbc54d
MANISH KUMAR <103636288+selfmanish01@users.noreply.github.com>
2024-01-01 11:22:01 +0530

    Delete promise directory

--- promise/promise.js
+++
@@ -1,6 +0,0 @@
-const promise = new promise(function (resolve, reject) {
-    console.log("Promise accepted")
-    setTimeout(function () {
-        console.log( " Async task 2 " );
-    },1000)
-})
